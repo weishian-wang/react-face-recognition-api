d243b121acdfd50a75ebaccff3f08863efd08aea
weishian-wang <weishianwang@gmail.com>
2019-02-15 17:26:23 -0500

    Change database settings

--- server.js
+++ server.js
@@ -14,12 +14,10 @@ const isAuth = require('./middleware/isAuth');
 const app = express();
 const port = process.env.PORT;
 const db = knex({
-  client: process.env.DB_CLIENT,
+  client: 'pg',
   connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
